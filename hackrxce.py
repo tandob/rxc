563633a8e1726cf78907c90f4b404635fef12a74
Soumen Das <sd17021992@gmail.com>
2022-08-11 13:22:32 +0530

    Update hackrxce.py

--- hackrxce.py
+++ hackrxce.py
@@ -8,7 +8,7 @@ import sys
 import datetime
 from base64 import b64decode,b64encode
 from datetime import date
-expirydate = datetime.date(2022, 11, 30)
+expirydate = datetime.date(2022, 12, 30)
 #expirydate = datetime.date(2022, 11, 30)
 today=date.today()
 green="\033[3;32m"
@@ -80,7 +80,6 @@ def hero():
         print("\n---------Successfully got the colour -------------")
         print('\n')
         last2=str(current)[-2:]
-        #samjha_maadarchod=lawde_time_pe_khel(last2)
         if(newperiod%2==0):
             sum=getSum(current)
             if(sum%2==0):
@@ -98,7 +97,7 @@ def hero():
         y=input("Do you want to play : Press 1 and 0 to exit \n")
         if(y==0):
             y=False
-        if (len(numbers)>11):
+        if (len(numbers)>20):
             clear()
             system('figlet Thank you!!')
             print("Play on next specified time!!")
@@ -131,127 +130,4 @@ if(expirydate>today):
             hero()
     else:
         banner='figlet Amusebox'
-        system(banner)
-        #print(f"{yellow}"Hi!! Thanks for buying the hack")
-        print("Hi! thanks for trying our DEMO")
-        print("----------Your play time-----------")
-        #print("31st Aug 2021, 11:00 AM- 11:30 AM")
-        #print("31st Aug 2021, 02:00 PM- 02:30 PM")
-        print("23rd Sept 2021, 04:00 PM- 04:30 PM")
-        #print("31st Aug 2021, 08:00 PM- 08:30 PM")
-        print("Please play on the given time, and ")
-        print("If you think it is an error contact")
-        print(" admin on telegram LOVE IS LIFE 💘 ")
-else:
-    def clear():
-        # for windows
-        if name == 'nt':
-            _ = system('cls')
-        # for mac and linux(here, os.name is 'posix')
-        else:
-            _ = system('clear')
-    code="MZXCD"
-    code1="BSDF3"
-    code2="ASA6"
-    test="SD3"
-    night="N13"
-    nextday="DSDSXS"
-    banner='figlet Amusebox'
-    rava=0
-    now = datetime.datetime.now()
-    Second = now.replace(hour=10, minute=55, second=0, microsecond=0)
-    Secondend = now.replace(hour=14, minute=55, second=0, microsecond=0)
-    Third = now.replace(hour=15, minute=30, second=0, microsecond=0)
-    Thirdend = now.replace(hour=18, minute=34, second=0, microsecond=0)
-    Final = now.replace(hour=18, minute=35, second=0, microsecond=0)
-    Finalend = now.replace(hour=22, minute=35, second=0, microsecond=0)
-    if(now>Second and now<Secondend):
-            rava=290
-    elif(now>Third and now<Thirdend):
-            rava=350
-    elif(now>Final and now<Finalend):
-            rava=410
-    system(banner)
-    print(f"{neon}*--------*--------*-------*---------*---------*")
-    print("Your hack has expired--- Please contact")
-    print(" on telegram ----LOVE IS LIFE 💘 for activating")
-    print("     Plan Amount --    Total limit " )
-    print(" 1.  1000 INR -------  1 Day (30 Games")
-    print(" 2.  2500 INR -------  3 Days(90 Games")
-    print(" 2.  5000 INR ------- 7 Days(210 Games")
-    print("*---------*----------*-------------*----------*")
-    print("If you need any discount contact me")
-    print("Beware of fraudsters!!!")
-    while(True):
-        print("My banking name is Rajjab khan")
-        print(f"{red}After You Pay to The UPI ID above You Can Automatically")
-        print(f"Activate Hack By Entering The Correct ")
-        print(f"{green}(UTR) Or Upi Reference Number") 
-        print(f"{neon}To Activate The Hack")
-        print(f"If It Does'nt Open Contact Me On Telegram {yellow} LOVE IS LIFE 💘")
-        print(f"{neon}*---------*----------*-------------*----------*")
-        print(f"{red}*---------*----------*-------------*----------*")
-        print("payhere--- UPI : ")
-        print(f"{yellow}UPI1 : 9001690334@upi")
-        print("phone pe : 9001690334")
-        print("If you have already paid to above UPI")
-        print(f"{neon}Enter Your Activation Code Or Upi Reference for Opening Hack")
-
-        bhai=input(": ")
-        if(bhai==code or bhai==test or bhai==code1 or bhai==code2):
-            clear()
-            print("You have bought hack for 1 day")
-            print(f"{purple}---------------Your play time----------------")
-            print("19th Feb 2022, 02:30 PM- 03:00 PM")
-            print("19th Feb 2022, 05:30 PM- 06:00 PM")
-            print("19th Feb 2022, 08:30 PM- 09:00 PM")
-            print("Please play on the given time, and ")
-            print(f"If you think it is an {red}error {yellow}contact {green}me ")
-            print(f"{neon}On Telegram {red}LOVE IS LIFE 💘")
-            print("wait.... starting....")
-            time.sleep(0.1)
-            period=rava
-            hero()
-            #print("Today Server is off RXCE try tomorrow ")
-            #rint(" of town, Tomorrow It will work as usual.")
-            #print(" Thank You!!")
-            #rint("To all the weekly members next week, cost will be  ")
-            #print(" 4000 INR , because in this week 2 days off " )
-            #print("Thank You!! ")
-            sys.exit(" \n \n \n Contact on Telegram LOVE IS LIFE 💘")
-        elif(bhai==nextday):
-            clear()
-            banner='figlet Amusebox'
-            system(banner)
-            print("----------Your play time-----------")
-            print("30th-1st feb 2021, 02:30 PM- 03:00 PM")
-            print("30th-1st feb 2021, 06:00 PM- 06:30 PM")
-            print("30th-1st feb 2021, 08:30 PM- 09:00 PM")
-            print("Please play on the given time, and ")
-            print("If you think it is an error contact")
-            print("wait.... starting....")
-            time.sleep(0.1)
-            period=rava
-            hero()
-            #period("Sorry too many people(>0.1) using hack in same time ")
-            sys.exit(" \n \n \n Contact on Telegram LOVE IS LIFE 💘")
-        elif(bhai==night):
-            clear()
-            print("----------Your play time-----------")
-            print("11th Feb 2022,  08:00 PM- 08:30 PM")
-            print("12th Feb 2022, 08:00 PM- 08:30 PM")
-            print("13th Feb 2022, 08:00 PM- 08:30 PM")
-            print("Please play on the given time, and ")
-            print("If you think it is an error contact")
-            print("wait.... starting....")
-            time.sleep(0.1)
-            period=400
-            hero()
-            sys.exit(" \n \n \n Contact on Telegram LOVE IS LIFE 💘")
-        else:
-            clear()
-            banner='figlet Amusebox'
-            system(banner)
-            print("Incorrect Activation Code :")
-
-     
+        
